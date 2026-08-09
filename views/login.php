964a34f6996543c51cf5bee03ee5be68078d2b76
coffelatte05 <ashchavacal05@gmail.com>
2026-08-09 16:42:14 -0600

    correcion de login

--- views/login.php
+++ views/login.php
@@ -1,38 +1,39 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>FindMyPaw</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-  <script src="../js/auth.js"></script>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FindMyPaw</title>
+    <link rel="stylesheet" href="../css/login.css">
+
 </head>
-<body class="container mt-5">
 
-  <h1>Bienvenido a FindMyPaw!</h1>
-  <h2>Iniciar Sesión</h2>
+<body>
 
-  <div class="alert alert-danger d-none" id="loginError"></div>
+    <div class="contenedor">
 
-  <form id="formLogin">
-    <input
-      type="email"
-      class="form-control mb-2"
-      name="correo"
-      id="correoLogin"
-      placeholder="Ingresa tu Correo">
+        <div id="login">
 
-    <input
-      type="password"
-      class="form-control mb-2"
-      name="password"
-      id="passwordLogin"
-      placeholder="Ingresa tu Contraseña">
+            <h1> Bienvenido a FindMyPaw !</h1>
 
-    <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
-    <a href="register.php" class="btn btn-secondary ms-2">Registrarse</a>
-  </form>
+            <h2>Iniciar Sesión</h2>
+
+            <input type="email" id="correoLogin" placeholder="Ingresa tu Correo">
+            <input type="password" id="passwordLogin" placeholder="Ingresa tu Contraseña">
+            <button type="button" id="btnIniciar">Iniciar Sesión</button>
+            <p id="mensajeR"></p>
+
+            <p>
+                ¿No tienes cuenta?
+             <button type="button" id="btnRegistro">Registrarse</button>
+            </p>    
+
+        </div>
+
+
+    <script src="../js/login.js"></script>
 
 </body>
+ 
 </html>
